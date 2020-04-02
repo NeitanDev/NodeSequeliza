170d57c56ad2d2efdc49325c9c8dadd125a8df98
NeitanDev <natan.miranda02@gmail.com>
2020-04-02 15:33:38 -0300

    Conexão com banco de dados e metode incersão de dados

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,9 @@
 const Sequlize = require('sequelize');
 const dbConfig = require('../config/database');
+const User = require('../models/User');
 
 const connection = new Sequlize(dbConfig);
+
+User.init(connection);
 
 module.exports = connection;
--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const routes = require('./routes');
 const app = express();
 
+require('./database/index');
+
 app.use(express.json());
 
 app.use(routes);
