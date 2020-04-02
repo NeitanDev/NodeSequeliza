0aa7aaf4a8fbfbd2df304ee3031a0b698997672a
NeitanDev <natan.miranda02@gmail.com>
2020-04-02 01:27:14 -0300

    codigo de conexão inconpleto 23min

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dialect: 'mysql',
+    host: 'localhost',
+    username: 'root',
+    password: 'root',
+    database: 'NodeSequelize',
+    define: {
+        timestamps: true,
+        underscored: true,
+    },
+};
---
+++ src/database/index.js
@@ -0,0 +1,4 @@
+const Sequlize = require('sequelize');
+const dbConfig = require('../config/database');
+
+const connection
